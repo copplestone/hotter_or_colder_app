27c513d471926c7ac7e756d3c5ffd02ac714c664
Grace Copplestone <gracecopplestone@gmail.com>
2014-01-28 23:41:43 +0000

    Added a double ==

--- app-mongo.rb
+++ app-mongo.rb
@@ -215,10 +215,11 @@ def update_weather(location)
 end
 
 def correct_date_check
-        if DateTime.now.day = 1
+        if DateTime.now.day == 1
             correct_date
         else
         end
+    return "Got through correct_date_check"
 end
 
 def correct_date
