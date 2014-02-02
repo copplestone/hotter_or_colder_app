267abb1f11481fbb666d6984999f6602b0623835
Grace Copplestone <gracecopplestone@gmail.com>
2014-02-02 00:27:00 +0000

    Changes to update_db

--- update_db.rb
+++ update_db.rb
@@ -47,15 +47,49 @@ def daily_forecast(location)
         @wind_tomorrow << f["S"].to_i
     end
 
-    tomorrow = WeatherData.new 
-        tomorrow.date = [DateTime.now.year, DateTime.now.month, DateTime.now.day+1]
-        tomorrow.location = location
-        tomorrow.temperature = @temp_tomorrow
-        tomorrow.rainfall = @rain_tomorrow
-        tomorrow.wind = @wind_tomorrow
-        tomorrow.cloud = @cloud_tomorrow
-    tomorrow.save
+    @tomorrow = WeatherData.new 
+        @tomorrow.date = [DateTime.now.year, DateTime.now.month, DateTime.now.day+1]
+        date_check
+        @tomorrow.location = location
+        @tomorrow.temperature = @temp_tomorrow
+        @tomorrow.rainfall = @rain_tomorrow
+        @tomorrow.wind = @wind_tomorrow
+        @tomorrow.cloud = @cloud_tomorrow
+    @tomorrow.save
 end
+
+def date_check
+        if DateTime.now.month == 1 && DateTime.now.day == 31
+            @tomorrow.date = [DateTime.now.year, 2, 1]
+        elsif DateTime.now.month == 2 && DateTime.now.day == 28
+            @tomorrow.date = [DateTime.now.year, 3, 1]
+        elsif DateTime.now.month == 3 && DateTime.now.day == 31
+            @tomorrow.date = [DateTime.now.year, 4, 1]
+        elsif DateTime.now.month == 4 && DateTime.now.day == 30
+            @tomorrow.date = [DateTime.now.year, 5, 1]
+        elsif DateTime.now.month == 5 && DateTime.now.day == 31
+            @tomorrow.date = [DateTime.now.year, 6, 1]
+        elsif DateTime.now.month == 6 && DateTime.now.day == 30
+            @tomorrow.date = [DateTime.now.year, 7, 1]
+        elsif DateTime.now.month == 7 && DateTime.now.day == 31
+            @tomorrow.date = [DateTime.now.year, 8, 1]
+        elsif DateTime.now.month == 8 && DateTime.now.day == 31
+            @tomorrow.date = [DateTime.now.year, 9, 1]
+        elsif DateTime.now.month == 9 && DateTime.now.day == 30
+            @tomorrow.date = [DateTime.now.year, 10, 1]
+        elsif DateTime.now.month == 10 && DateTime.now.day == 31
+            @tomorrow.date = [DateTime.now.year, 11, 1]
+        elsif DateTime.now.month == 11 && DateTime.now.day == 30
+            @tomorrow.date = [DateTime.now.year, 12, 1]
+        elsif DateTime.now.month == 12 && DateTime.now.day == 31
+            @tomorrow.date = [DateTime.now.year, 1, 1]
+        else
+        end
+    return
+end
+
+#Used a global variable in date_check @tomorrow but then once this has been 
+# altered to correct the date this needs to communicate to daily_forecast
 
 daily_forecast("bristol")
 daily_forecast("cambridge")
